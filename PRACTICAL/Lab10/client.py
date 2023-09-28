9bcc1cda3e8e7bb6dadc56b8687f9607276248e2
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2023-09-28 17:44:40 +0700

    Web Year 2

--- PRACTICAL/Lab10/client.py
+++ PRACTICAL/Lab10/client.py
@@ -1,39 +1,57 @@
 import requests
 
-url = "http://161.246.5.61:11328/students/html"
-url2 = "http://161.246.5.61:11328/students/123456"
 
+def post_pram(name, surname, ID):
+    url = "http://161.246.5.61:11328/students/newForm?student_name={}&student_surname={}&student_id={}".format(name, surname, ID)
+    response = requests.post(url, json={"name": name, "surname": surname, "ID": ID})
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Error: ", response.status_code)
 
-response = requests.post("http://161.246.5.61:11328/students/newForm?student_name=John&student_surname=Smith&student_id=1234")
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Error: ", response.status_code)
+def post_link(name, surname, ID):
+    url = "http://161.246.5.61:11328/students/new/{}/{}/{}/".format(name, surname, ID)
+    response = requests.post(url)
     
-response = requests.post("http://161.246.5.61:11328/students/new/abc/abc/12345")
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Error: ", response.status_code)
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Error: ", response.status_code)
     
-response = requests.post("http://161.246.5.61:11328/students/new/", json={"name": "John", "surname": "Smith", "ID": "123456"})
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Error: ", response.status_code)
-
-
-
-response = requests.get(url)
-
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Error: ", response.status_code)
+def post_json(name, surname, ID):
+    url = "http://161.246.5.61:11328/students/new/"
+    response = requests.post(url, json={"name": name, "surname": surname, "ID": ID})
     
-response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Error: ", response.status_code)
 
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Error: ", response.status_code)
+def get_html():
+    url = "http://161.246.5.61:11328/students/html"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Error: ", response.status_code)
+
+def get_html_id(ID):
+    url = "http://161.246.5.61:11328/students/html/{}".format(ID)
+    
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Error: ", response.status_code)
+        
+
+post_pram("John", "Doe", 1234)
+post_link("John", "Doe", 12345)
+post_json("John", "Doe", 12346)
+
+get_html()
+get_html_id(1234)
+get_html_id(12345)
+get_html_id(12346)
